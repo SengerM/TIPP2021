b53320c3c2131abd92102507e7a572d4b9968678
Matias Senger <m.senger@hotmail.com>
2021-05-14 10:40:53 +0200

    improved header and minor stuff

--- js/header.js
+++ js/header.js
@@ -3,14 +3,14 @@ window.onscroll = function() {myFunction()};
 var header = document.getElementById("presentation_header");
 var sticky = header.offsetTop;
 
-header.classList.add("non_sticked_header");
+var sticked_header = header.cloneNode(true);
+sticked_header.id = "sticked_presentation_header";
+document.body.appendChild(sticked_header);
 
 function myFunction() {
 	if (window.pageYOffset > sticky) {
-		header.classList.add("sticked_header");
-		header.classList.remove("non_sticked_header");
+		sticked_header.classList.add("show_header");
 	} else {
-		header.classList.remove("sticked_header");
-		header.classList.add("non_sticked_header");
+		sticked_header.classList.remove("show_header");
 	}
 }
